fe6db335246d4100399d533e4c0d091c14541f5c
Anthony DiPietrantonio <antdp425@aol.com>
2020-04-02 17:19:31 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,12 +2,13 @@
 class Anagram
 attr_accessor :word
 
-def initialize(word)
-  @word = word
-end
+  def initialize(word)
+    @word = word
+  end
 
-def match(anagrams)
-  
-end
+  def match(anagrams)
+    word = self.word.split("").sort
+    anagrams.select{|a| a.split("").sort == word}  
+  end
 
 end
